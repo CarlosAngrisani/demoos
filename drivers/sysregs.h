2f089fb7d34b3426fd8de3ab51128803047c4fed
Carloa <c.angrisani1@campus.unimib.it>
2025-10-28 15:03:15 +0100

    [refactor/sysregs-startS] Rimosso SCR_EL3 da sysregs.h (non necessario su Raspberry Pi 3 che parte sempre da EL2), modificato SPSR_EL3 in SPSR_EL2 (per lo stesso motivo); eliminata riga duplicata in start.S

--- drivers/sysregs.h
+++ drivers/sysregs.h
@@ -23,16 +23,7 @@
 #define HCR_VALUE			HCR_RW
 
 // ***************************************
-// SCR_EL3, Secure Configuration Register (EL3), Page 2648 of AArch64-Reference-Manual.
-// ***************************************
-
-#define SCR_RESERVED	    		(3 << 4)
-#define SCR_RW				(1 << 10)
-#define SCR_NS				(1 << 0)
-#define SCR_VALUE	    	    	(SCR_RESERVED | SCR_RW | SCR_NS)
-
-// ***************************************
-// SPSR_EL3, Saved Program Status Register (EL3) Page 389 of AArch64-Reference-Manual.
+// SPSR_EL2, Saved Program Status Register (EL2) Page 383 of AArch64-Reference-Manual.
 // ***************************************
 
 #define SPSR_MASK_ALL 			(7 << 6)
